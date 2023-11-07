be23ac58a26314b2b2247bfd21af815f427dc131
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-07 20:36:26 +0100

    update constructor class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,28 @@ class BaseModel:
 
     # public instance id
 
-    def __init__(self):
-        """constructor method"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """constructor method
+        Args:
+            id(str): unique id of an object
+            create_at(datetime): date and time  of creation new  instance
+            updated_at(datetime): dat and time of
+            updating already created instance
+        """
+        # case1 : kwargs is not empty
+        if kwargs:
+            if not isinstance(kwargs, dict):
+                return
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ string representation of an object"""
