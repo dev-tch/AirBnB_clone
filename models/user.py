cf15c15291fa7f920ec73e73355501f0ef55ee6b
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-10 00:38:04 +0100

    fix user class

--- models/user.py
+++ models/user.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ module with one class User"""
+from models.base_model import BaseModel
 
 
-class User:
+class User(BaseModel):
     """implement class"""
     # public attributes
     email = ""
