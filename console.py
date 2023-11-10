9b23bbeeca7fd3095fd77d4d89ef365b3bb81542
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-10 01:06:59 +0100

    fix console

--- console.py
+++ console.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 """entry point of the command interpreter"""
 from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models import storage
 import cmd
 import shlex
