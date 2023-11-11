2d4a8189ef404dddf1b8d47d0faab399a0bbbd37
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-11 02:25:15 +0100

    optional tasks console

--- console.py
+++ console.py
@@ -10,6 +10,7 @@ from models.review import Review
 from models import storage
 import cmd
 import shlex
+import re  # import regex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +20,7 @@ class HBNBCommand(cmd.Cmd):
                           "City", "Amenity", "Place",
                           "Review"
                           ]
+    custom_procs = ["all", "count", "show", "destroy", "update"]
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
@@ -89,6 +91,7 @@ class HBNBCommand(cmd.Cmd):
     def do_all(self, arg):
         """Print all string representation of all instances"""
         args = shlex.split(arg)
+        print(args)
         if not args:
             # show all instances in storageFile
             str_obj_list = [str(ref_obj) for ref_obj in storage.all().values()]
@@ -111,6 +114,7 @@ class HBNBCommand(cmd.Cmd):
         elif len(args) < 2:
             print("** instance id missing **")
         else:
+            print(args)
             str_object_id = ".".join(map(str, args[:2]))
             # search in storage __objects for this id
             ref_obj_found = storage.all().get(str_object_id, None)
@@ -141,6 +145,63 @@ class HBNBCommand(cmd.Cmd):
                     ref_obj_found.save()
                 except ValueError:
                     pass
+
+    def do_count(self, arg):
+        """Retrieve the number of instances of a class"""
+        args = shlex.split(arg)
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.defined_class_list:
+            print("** class doesn't exist **")
+        else:
+            # count instances of the specified class
+            count = sum(1 for obj in storage.all().values()
+                        if type(obj).__name__ == args[0])
+            print(count)
+
+    def default(self, args):
+        """ execute custom command"""
+        # init local variables
+        name_cls = ""
+        name_proc = ""
+        params = ""
+        method_name = ""
+        flag = True
+        # regex to extract elements of method
+        data = re.findall(r"(\w+)\.(\w+)\((.*?)\)", args)
+        if data:
+            name_cls = data[0][0]
+            name_proc = data[0][1]
+            params = data[0][2]
+        if params and name_proc in HBNBCommand.custom_procs[0:2]:
+            flag = False
+        elif name_proc in HBNBCommand.custom_procs[2:4]:
+            if not re.match(r"\"[a-zA-Z0-9-]+\"", params):
+                flag = False
+        elif name_proc == HBNBCommand.custom_procs[4]:
+            # <id>, <attribute name>, <attribute value>
+            reg1 = r"\"[a-zA-Z0-9-]+\", \"\w+\", \"\w+\""
+            # <id>, <dictionary representation>
+            reg2 = r"\"[a-zA-Z0-9-]+\", {.*}"
+            reg1_ok = False
+            reg2_ok = False
+            if re.match(reg1, params):
+                reg1_ok = True
+                params = params.replace(",", " ")
+            elif re.match(reg2, params):
+                reg2_ok = True
+                params = params.replace(",", " ", 1)
+            if not reg1_ok and not reg2_ok:
+                flag = False
+        if (flag and name_cls in HBNBCommand.defined_class_list and
+                name_proc in HBNBCommand.custom_procs):
+            method_name = "do_{}".format(name_proc)
+            if (hasattr(self, method_name) and
+                    callable(getattr(self, method_name))):
+                method = getattr(self, method_name)
+                return method(f"{name_cls} {params}")
+        print("*** Unknown syntax: {}".format(args))
+        return False
 
 
 if __name__ == '__main__':
