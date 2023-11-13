2efe0e94c0e127dd8c1c64eca8c1deb54da0eb24
dev-tch <tijeni.chaabeni@gmail.com>
2023-11-14 00:33:56 +0100

    test basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -14,6 +14,7 @@ class TestBaseModel(unittest.TestCase):
     implement test cases
     """
     objBaseModel = None
+    objBaseModel2 = None
     #  data test prepare and clean
     # setUpClass: common to all instances of test cases
 
@@ -23,11 +24,14 @@ class TestBaseModel(unittest.TestCase):
         cls.objBaseModel = BaseModel()
         cls.objBaseModel.name = "Tc"
         cls.objBaseModel.age = 32
+        cls.objBaseModel2 = BaseModel(hobby="music", Job="software")
+        cls.objBaseModel2.gender = "Male"
 
     @classmethod
     def teardown(cls):
         """run after end of class run"""
         del cls.objBaseModel
+        del cls.objBaseModel2
 
     def tearDown(self):
         """called immediately after the test method has been called
@@ -53,6 +57,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(base_model.BaseModel.__str__.__doc__)
 
     def test_save(self):
+        """ test method save """
         dt_creat_before = self.objBaseModel.created_at
         dt_update_before = self.objBaseModel.updated_at
         self.objBaseModel.save()
@@ -60,3 +65,42 @@ class TestBaseModel(unittest.TestCase):
         dt_creat_after = self.objBaseModel.created_at
         self.assertGreater(dt_update_after, dt_update_before)
         self.assertEqual(dt_creat_before, dt_creat_after)
+
+    def test_to_dict(self):
+        """ test method to dict"""
+        # Rule_1: returns a dictionary containing all
+        #   keys/values of __dict__ of the instance
+        # ---------------- test instance with dictionary ---------------
+
+        result_dict = self.objBaseModel2.to_dict()
+        self.assertIsInstance(result_dict, dict)
+        # Rule_2: by using self.__dict__,
+        #   only instance attributes set will be returned
+        # Rules3: a key __class__ must be added to
+        #   this dictionary with the class name of the object
+        expected_dict = {"hobby": "music", "job": "software",
+                         "gender": "Male", "__class__": "BaseMode"}
+        sorted_result_dict = dict(sorted(result_dict.items()))
+        expected_dict = dict(sorted(result_dict.items()))
+        self.assertDictEqual(expected_dict, result_dict)
+
+        # ---------------- test instance without dict --------------------
+
+        result_dict = self.objBaseModel.to_dict()
+        self.assertIsInstance(result_dict, dict)
+        expected_keys = sorted(['name', 'age', 'created_at',
+                                'updated_at', 'id', '__class__'])
+        self.assertEqual(sorted(list(result_dict.keys())), expected_keys)
+        self.assertEqual(result_dict["__class__"], 'BaseModel')
+        # created_at and updated_at
+        # must be converted to string object in ISO format
+        self.assertIsInstance(result_dict['created_at'], str)
+        self.assertIsInstance(result_dict['updated_at'], str)
+        dt_cr_iso = datetime.strptime(result_dict['created_at'],
+                                      "%Y-%m-%dT%H:%M:%S.%f")
+        dt_up_iso = datetime.strptime(result_dict['updated_at'],
+                                      "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertIsInstance(dt_cr_iso, datetime)
+        self.assertIsInstance(dt_up_iso, datetime)
+        self.assertEqual(self.objBaseModel.created_at, dt_cr_iso)
+        self.assertEqual(self.objBaseModel.updated_at, dt_up_iso)
